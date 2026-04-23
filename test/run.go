c45f6fa97869abb7ee2269be141a46fac90608c3
wutomi <utomiwisdom2@gmail.com>
2026-04-23 17:46:13 +0100

    all

--- test/run.go
+++ test/run.go
@@ -77,15 +77,15 @@ import (
 // 	fmt.Println(piscine.IsCapitalized(""))
 // }
 
-// func main() {
-//     fmt.Println(piscine.Itoa(12345))
-//     fmt.Println(piscine.Itoa(0))
-//     fmt.Println(piscine.Itoa(-1234))
-//     fmt.Println(piscine.Itoa(987654321))
-// }
-
 func main() {
-	fmt.Print(piscine.LastWord("this        ...       is sparta, then again, maybe    not"))
-	fmt.Print(piscine.LastWord(" lorem,ipsum "))
-	fmt.Print(piscine.LastWord(" "))
+    fmt.Println(piscine.Itoa(12345))
+    fmt.Println(piscine.Itoa(0))
+    fmt.Println(piscine.Itoa(-1234))
+    fmt.Println(piscine.Itoa(987654321))
 }
+
+// func main() {
+// 	fmt.Print(piscine.LastWord("this        ...       is sparta, then again, maybe    not"))
+// 	fmt.Print(piscine.LastWord(" lorem,ipsum "))
+// 	fmt.Print(piscine.LastWord(" "))
+// }
