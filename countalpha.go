a6cd6af4bfb1f32bdc0ce666e2d398ff61f24e11
wutomi <utomiwisdom2@gmail.com>
2026-04-24 06:35:17 +0100

    success

--- countalpha.go
+++ countalpha.go
@@ -11,21 +11,20 @@ func CountAlpha(s string) int {
 	count := 0
 	for _, ch := range s {
 		if (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') {
-			count ++
+			count++
 		}
 	}
-	return count 
+	return count
 }
 
-
-// package piscine 
+// package piscine
 
 // func CountAlpha(s string) int {
-// 	count = 0 
+// 	count := 0
 // 	for _, ch := range s {
 // 		if (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') {
 // 			count ++
 // 		}
 // 	}
-// 	return count 	
+// 	return count
 // }
